db052f9ba6c7136044baa72c9a5bd42a7d50ffd9
Mah <timohaarsentiev1@gmail.com>
2025-05-20 17:35:30 +0300

    Code Update

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int accountBal = 200;
         int deposit = 1400;
+        int bonus = 100;
         if (deposit > 999) {
-            accountBal = deposit + deposit / 100 + accountBal;
+            accountBal = deposit + deposit / bonus + accountBal;
         } else {
             accountBal = deposit + accountBal;
         }
